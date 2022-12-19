e5b184c0316a7f501debdf279eca034e9e3d7f3d
kez <kerrialbeckettnewham@gmail.com>
2022-12-19 15:33:36 +0100

    add id to admin

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -6,6 +6,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
@@ -18,7 +19,7 @@ class UserCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            TextField::new('id'),
+            IdField::new('id'),
             TextField::new('firstName'),
             TextField::new('lastName'),
             TextField::new('email'),
