0320bbef39f95af09937b728e3da47612fc8162e
kez <kerrialbeckettnewham@gmail.com>
2023-01-05 18:22:38 +0100

    fix cs

--- src/Controller/Controller/FeedController.php
+++ src/Controller/Controller/FeedController.php
@@ -4,7 +4,6 @@ declare(strict_types = 1);
 
 namespace App\Controller\Controller;
 
-use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,11 +11,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FeedController extends AbstractController
 {
-    public function __construct(
-        private readonly EventRepository $eventRepository
-    ) {
-    }
-
     #[Route(path: '/feed', name: 'feed')]
     public function index(Request $request): Response
     {
