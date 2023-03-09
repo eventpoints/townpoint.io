7570179f235d63fee93506d63fca8b91981a64c9
kez <kerrialbeckettnewham@gmail.com>
2023-03-09 09:48:53 +0100

    fix InstanceOfExtension

--- src/Twig/Extension/InstanceOfExtension.php
+++ src/Twig/Extension/InstanceOfExtension.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace App\Twig\Extension;
 
 use ReflectionClass;
+use ReflectionException;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
@@ -12,13 +13,14 @@ class InstanceOfExtension extends AbstractExtension
 {
     public function getFilters(): array
     {
-        return [new TwigFilter('instanceof', function ($object, string $class): bool {
-            return $this->isInstanceOf($object, $class);
+        return [new TwigFilter('instanceof', function ($object): bool {
+            return $this->isInstanceOf($object);
         })];
     }
 
-    public function isInstanceOf($object, string $class): bool
+    public function isInstanceOf(Object $object): bool
     {
+        $class = get_class($object);
         $reflectionClass = new ReflectionClass($class);
 
         return $reflectionClass->isInstance($object);
